549bb8db76c0cf696e636c43e0e72f6b964ea8d9
Deep Parekh <dparekh@tripadvisor.com>
2017-04-12 20:08:03 -0400

    Added home view

---
+++ youtube_tv_clone/youtube_tv_clone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  youtube_tv_clone
+//
+//  Created by Deep Parekh on 4/12/17.
+//  Copyright © 2017 Parekh Bros. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ youtube_tv_clone/youtube_tv_clone/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  youtube_tv_clone
+//
+//  Created by Deep Parekh on 4/12/17.
+//  Copyright © 2017 Parekh Bros. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
